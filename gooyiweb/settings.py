b489562d2a9cf6440507f6ed541b062b9bdd63c9
Taewan Goo <guti@Taewanui-MacBook-Pro.local>
2018-10-03 21:47:14 +0900

    Login Function

--- gooyiweb/settings.py
+++ gooyiweb/settings.py
@@ -124,17 +124,17 @@ TEMPLATES = [
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 
 # RDS 세팅
-        #'ENGINE': 'django.db.backends.mysql',
-        #'NAME': 'jins_database',
-        #'USER': 'jins',
-        #'PASSWORD': '04090409',
-        #'HOST': 'mydbinstance-maria.csqkumwjtibh.ap-northeast-2.rds.amazonaws.com',
-        #'PORT': '3306'
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'jins_database',
+        'USER': 'jins',
+        'PASSWORD': '04090409',
+        'HOST': 'mydbinstance-maria.csqkumwjtibh.ap-northeast-2.rds.amazonaws.com',
+        'PORT': '3306'
 
 # 로컬 세팅
 #        'ENGINE': 'django.db.backends.mysql',
