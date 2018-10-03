fb867f8d2fdcdb8e638a208a5df3017d996a3469
Taewan Goo <guti@Taewanui-MacBook-Pro.local>
2018-10-03 21:52:18 +0900

    LOGIN

--- gooyiweb/settings.py
+++ gooyiweb/settings.py
@@ -124,11 +124,11 @@ TEMPLATES = [
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    'default': {
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 
-# RDS 세팅
+# # RDS 세팅
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'jins_database',
         'USER': 'jins',
